77072663875618e8fdaeab1d7b4c5a23f9d19a71
Josh <joshbwahahahaha@gmail.com>
2024-12-21 17:23:39 +0800

    Create OE# 5.py

---
+++ Other Evidence/OE# 5.py
@@ -0,0 +1,121 @@
+Game Title: MLBB
+
+Game object 1: Atlas
+attributes
+- Name
+- Map
+- Type of game
+- Battle points
+- Diamonds
+- Fragments
+- Tickets
+- Origin: Northern Vale
+- Role: Tank
+- Lane: Roaming
+- Species: Abysmal sea creature
+- Specialty: Crowd control/Initiator
+- Gender: Male
+- Health
+- Weapon: Mecha Sentry
+- Damage: Magic
+- Skin: Mecha Infernus
+- Skill resource: Mana
+- Fighting style
+- Ability
+- Speed
+
+methods:
+- Basic Attack type: melee
+- Passive: Frigid Breath
+- First skill: Annihilate
+- Second skill: Perfect Match
+- Ultimate: Fatal Links
+- Battle spell: Flicker
+
+Game object 2: Terizla
+attributes
+- Name
+- Map
+- Type of game
+- Battle points
+- Diamonds
+- Fragments
+- Tickets
+- Origin: Monian Empire
+- Lane: Exp lane
+- Species: Human
+- Specialty: Burst/Crowd contorl
+- Gender: Male
+- Role: Tank
+- Health
+- Weapon: Power infused hammer
+- Damage: Physical
+- Skin: Flame of judgement
+- Skill resource: Mana
+- Fighting Style
+
+methods:
+- Basic Attack type: melee
+- Passive: Body of smith
+- First skill: Revenge strike
+- Second skill: Execution strike
+- Ultimate: Penalty zone
+- Battle Spell: Vengeance
+
+Game object 3: Hayabusa
+attributes
+- Name
+- Map
+- Type of game
+- Battle points
+- Diamonds
+- Fragments
+- Tickets
+- Origin: Cadia Riverlands
+- Lane: Jungling
+- Species: Human
+- Specialty: Chase/Burst
+- Gender: Male
+- Role: Assassin
+- Health
+- Weapon: Katana, Kunai
+- Damage: Physical
+- Skin: Experiment 21
+- Skill resource: Energy
+- Fighting style: Ninjutsu
+
+methods:
+- Trace of Shadow
+- Phantom Shuriken
+- Quad Shadow
+- Shadow Kill
+- Retribution
+
+Game object 4: Ixia
+attributes
+- Name
+- Map
+- Type of game
+- Battle points
+- Diamonds
+- Fragments
+- Tickets
+- Origin: Agella Drylands
+- Lane: Gold lane
+- Species: Human
+- Specialty: Finisher/Damage
+- Gender: Female
+- Role: Marksman
+- Health
+- Weapon: Starlium-powered weapon
+- Damage: Physical
+- Skin: Dynamic Streak
+- Skill resources: Mana
+- Fighting style
+
+methods:
+- Passive: Siphon Starlium
+- First skill: Dual Beam
+- Second skill: Star Helix
+- Ultimate: Full Barrage
+- Spell: Inspire
