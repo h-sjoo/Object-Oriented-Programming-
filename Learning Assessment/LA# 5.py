3ccce63c02524f348bebabec4ab22f4538ad3a05
Josh <joshbwahahahaha@gmail.com>
2024-12-21 16:33:58 +0800

    .

--- Learning Assessment/LA# 5.py
+++ Learning Assessment/LA# 5.py
@@ -1,7 +1,7 @@
 class mobaylhero():
     def __init__(self, name, role):
         self.name = name
-        self.role = role
+        self.role = rolen
 
     def describe(self):
         print(f"{self.name} is a/an {self.role} hero.")
